51e9b61f6f829a0cc06a0c33aa528200a5688233
professorik <36134599+professorik@users.noreply.github.com>
2024-04-23 12:23:59 +0300

    opa

--- main.py
+++ main.py
@@ -4,21 +4,12 @@ import numpy as np
 import time
 
 
-def problem_1():
+if __name__ == '__main__':
     start = time.time()
-    model = waves.Model(beta=0, d=0.01, L=0.05, _lambda=0.0005)
-    table = model.solve()
-    # Prints 9.776944875717163
+    model = waves.Model(beta=np.pi / 2, d=0.01, L=0.05, _lambda=0.0005, K=120, N=50)
+    table = model.solve(w=4)
     print(time.time() - start)
 
-    utils.heatmap(table, length=1, time=1)
-    #utils.show_plot(table[0], length=1, l='-g+', label='Initial values')
-    #utils.show_plot(table[-1], length=1, label='50 points')
-    #utils.show_plot(table_2[-1], length=1, l='-r+', label='100 points')
-    utils.plt.legend()
+    utils.heatmap_waves(table, w=4*0.05, h=0.2)
+    #utils.plt.legend()
     utils.plt.show()
-    # utils.print_table(table)
-
-
-if __name__ == '__main__':
-    problem_1()
